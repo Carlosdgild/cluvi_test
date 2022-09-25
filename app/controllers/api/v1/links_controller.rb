0fb4f912966cbc2a4e254284d18e050d8bf0dc6e
carlosdgild <carlosdgild@gmail.com>
2022-09-25 18:08:40 -0400

    Add LinksController

---
+++ app/controllers/api/v1/links_controller.rb
@@ -0,0 +1,16 @@
+module Api
+  module V1
+    #
+    # Main Links controller class
+    #
+    class LinksController < ApplicationController
+      def show
+        head
+      end
+
+      def create
+        head
+      end
+    end
+  end
+end
