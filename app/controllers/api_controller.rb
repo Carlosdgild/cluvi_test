c5e0cf67e027cad8ef38e068dfc6d488b7d6955b
carlosdgild <carlosdgild@gmail.com>
2022-09-25 17:37:48 -0400

    Initial Setup

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+#
+# Main API class wrapper
+#
+class ApiController < ApplicationController
+  rescue_from(TypeError) { |error| handle_type_error_error(error) }
+  rescue_from(ArgumentError) { |error| handle_argument_error(error) }
+  rescue_from(ActiveRecord::RecordNotFound) { |error| handle_active_record_not_found_error(error) }
+  rescue_from(ActiveRecord::RecordInvalid) { |error| render_record_not_valid_error(error) }
+  rescue_from(ActiveRecord::RecordNotUnique) { |error| handle_active_record_not_unique_error(error) }
+  rescue_from(ActiveRecord::StatementInvalid) { |error| handle_active_record_statement_invalid_error(error) }
+  rescue_from(ActionController::ParameterMissing) { |error| handle_action_controller_parameter_missing_error(error) }
+  rescue_from(ActionController::RoutingError) { |error| handle_action_controller_routing_error(error) }
+  rescue_from(ActiveModel::ForbiddenAttributesError) { |error| handle_active_model_forbidden_attributes_error(error) }
+
+  #
+  # Raises no route
+  #
+  # @return [ActionController::RoutingError]
+  def raise_no_matching_route!
+    raise ActionController::RoutingError, "Route does not match #{request&.request_method} '/#{params[:unmatched_route_error]}'"
+  end
+
+  private
+
+  def render_error_response(message = "An error has occurred", status_code = :bad_request, code = 0, _errors = [])
+    render json: {
+      message: message,
+      code: code
+    }, status: status_code
+  end
+
+  def handle_argument_error(error)
+    logger.error(error&.message)
+
+    render_error_response(error&.message, :bad_request, 1)
+  end
+
+  def handle_type_error_error(error)
+    logger.error(error&.message)
+
+    render_error_response(error&.message, :bad_request, 2)
+  end
+
+  def handle_active_record_not_found_error(error)
+    logger.error(error&.message)
+
+    render_error_response("Could not find resource", :not_found, 4)
+  end
+
+  def handle_active_record_statement_invalid_error(error)
+    logger.error(error&.message)
+
+    render_error_response("Invalid statement in field", :unprocessable_entity, 3)
+  end
+
+  def handle_active_model_forbidden_attributes_error(error)
+    logger.error(error&.message)
+
+    render_error_response("Forbidden attributes in record", :unprocessable_entity, 5)
+  end
+
+  def handle_action_controller_parameter_missing_error(error)
+    logger.error("Missing parameter #{error&.param}")
+
+    render_error_response("Missing parameter #{error&.param}", :bad_request, 6)
+  end
+
+  def handle_action_controller_routing_error(error)
+    logger.error(error&.message)
+
+    render_error_response(error&.message, :bad_request, 7)
+  end
+
+  def handle_active_record_not_unique_error(error)
+    logger.error(error&.message)
+
+    render_error_response("A record already exists", :bad_request, 8)
+  end
+
+  def render_record_not_valid_error(error)
+    logger.error(error&.message)
+
+    render_error_response(error&.message, :unprocessable_entity, 9, error.record&.errors&.messages)
+  end
+end
---
+++ config/application.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require_relative "boot"
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+# require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_view/railtie"
+# require "action_cable/engine"
+# require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module CluviApi
+  #
+  # Main application class
+  #
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false
+    end
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+
+    # Adding manually some middlewares needed
+    # https://guides.rubyonrails.org/api_app.html#using-session-middlewares
+    # This also configures session_options for use below
+    config.session_store :cookie_store, key: "api_session"
+    # Required for all session management (regardless of session_store)
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Flash
+  end
+end
